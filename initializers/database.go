ddd2d7712fc4026d530ffd2cd58f62a2365fa1bc
TejasGhate <tejasghatte5@gmail.com>
2024-12-23 00:36:27 +0530

    feat: database and migrations init

--- initializers/database.go
+++ initializers/database.go
@@ -1,6 +1,25 @@
 package initializers
 
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
 
 func ConnectDB() {
-	
+	var err error
+
+	dsn := fmt.Sprintf(CONFIG.DB_URL)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to Connect to the database")
+	} else {
+		log.Println("Connected to database!")
+	}
 }
--- initializers/migrations.go
+++ initializers/migrations.go
@@ -1,5 +1,20 @@
 package initializers
 
+import (
+	"log"
+	"fmt"
+
+)
+
 func DBMigrate() {
-	
+	if DB == nil {
+		fmt.Println("Database not connected")
+	}
+
+    err := DB.AutoMigrate(
+	)
+
+	if err != nil {
+		log.Fatal("Failed to run migrations")
+	}
 }
