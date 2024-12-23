6a5567785f5a81375b1274c8f8ed27ab72e81866
TejasGhate <tejasghatte5@gmail.com>
2024-12-23 11:16:55 +0530

    fix: minor fixes

--- models/video_model.go
+++ models/video_model.go
@@ -4,13 +4,14 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 type Video struct {
 	ID            uuid.UUID      `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
-	Creator 	  string 		 `gorm:"type:varchar(255)" json:"creator"`
+	Creator       string         `gorm:"type:varchar(255)" json:"creator"`
 	Title         string         `gorm:"type:varchar(255)" json:"title"`
 	Description   string         `gorm:"type:text" json:"description"`
-	ThumbnailURLs string`gorm:"type:text[]" json:"thumbnailURLs"`
-	PublishedAt     time.Time      `gorm:"default:current_timestamp" json:"createdAt"`
+	ThumbnailURLs pq.StringArray `gorm:"type:text[]" json:"thumbnailURLs"`
+	PublishedAt   time.Time      `gorm:"default:current_timestamp" json:"publishedAt"`
 }
