bb70334cb837db468f12304b12112957c5646075
TejasGhate <tejasghatte5@gmail.com>
2024-12-23 13:46:21 +0530

    fix: fixed model

--- models/video_model.go
+++ models/video_model.go
@@ -9,9 +9,11 @@ import (
 
 type Video struct {
 	ID            uuid.UUID      `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
+	VideoID       string         `gorm:"uniqueIndex;not null" json:"videoID"`
 	Creator       string         `gorm:"type:varchar(255)" json:"creator"`
 	Title         string         `gorm:"type:varchar(255)" json:"title"`
 	Description   string         `gorm:"type:text" json:"description"`
 	ThumbnailURLs pq.StringArray `gorm:"type:text[]" json:"thumbnailURLs"`
-	PublishedAt   time.Time      `gorm:"default:current_timestamp" json:"publishedAt"`
+	PublishedAt   time.Time      `gorm:"" json:"publishedAt"`
+	CreatedAt     time.Time      `gorm:"default: current_timestamp" json:"createdAt"`
 }
