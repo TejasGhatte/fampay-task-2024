f9f4d17413363256bb119adefe2d5ac1e954ddaf
TejasGhate <tejasghatte5@gmail.com>
2024-12-23 13:46:32 +0530

    feat: improved fetching

--- routines/video.go
+++ routines/video.go
@@ -15,12 +15,13 @@ import (
 
 var (
 	query      = flag.String("query", "football", "Search term")
-	maxResults = flag.Int64("max-results", 100, "Max YouTube results")
+	maxResults = flag.Int64("max-results", 25, "Max YouTube results")
 )
 
 const developerKey = "API_KEY"
 
 func FetchVideos(){
+	startTime := time.Now()
 	flag.Parse()
 
 	// Initialize YouTube service
@@ -35,39 +36,57 @@ func FetchVideos(){
 	if err != nil {
 		log.Printf("Error fetching and storing videos: %v", err)
 	}
+
+	endTime := time.Now()
+	fmt.Println("Time taken to fetch and store videos:", endTime.Sub(startTime))
 	fmt.Println("Videos fetched and stored successfully")
 }
 
 func fetchAndStoreVideos(service *youtube.Service) error {
+
+	lastFetchTime := time.Now().Add(-24 * time.Hour)
+	var lastVideo models.Video
+	if err := initializers.DB.Order("published_at desc").First(&lastVideo).Error; err == nil {
+		lastFetchTime = lastVideo.PublishedAt
+	}
 	// Make the API call to YouTube
 	call := service.Search.List([]string{"id", "snippet"}).
 		Q(*query).
 		Type("video").
 		MaxResults(*maxResults).
-		Order("date")// Order by date to get the latest videos
+		Order("date").
+		PublishedAfter(lastFetchTime.Format(time.RFC3339))
+		// Order by date to get the latest videos
 
 	response, err := call.Do()
 	if err != nil {
 		return fmt.Errorf("error calling Search.List: %v", err)
 	}
 
+	var videos []models.Video
 	for _, item := range response.Items {
-		publishedAt, err := time.Parse(time.RFC3339, item.Snippet.PublishedAt)
-		if err != nil {
-			return fmt.Errorf("error parsing publishedAt: %v", err)
+		// Check for duplicates
+		existingVideo := models.Video{}
+		if err := initializers.DB.Where("video_id = ?", item.Id.VideoId).First(&existingVideo).Error; err == nil {
+			continue
 		}
-
+	
+		publishedAt, _ := time.Parse(time.RFC3339, item.Snippet.PublishedAt)
 		video := models.Video{
-			Title : item.Snippet.Title,
-			Description :item.Snippet.Description,
-			PublishedAt : publishedAt,
-			ThumbnailURLs : []string{item.Snippet.Thumbnails.Default.Url},
+			Creator:      item.Snippet.ChannelTitle,
+			VideoID:      item.Id.VideoId,
+			Title:        item.Snippet.Title,
+			Description:  item.Snippet.Description,
+			PublishedAt:  publishedAt,
+			ThumbnailURLs: []string{item.Snippet.Thumbnails.Default.Url},
 		}
-		fmt.Println(video.Title)
-
-		// Insert video details into the database
-		if err := initializers.DB.Create(&video).Error; err != nil {
-			return fmt.Errorf("error inserting video into database: %v", err)
+	
+		videos = append(videos, video)
+	}
+	
+	if len(videos) > 0 {
+		if err := initializers.DB.Create(&videos).Error; err != nil {
+			return fmt.Errorf("error inserting videos into database: %v", err)
 		}
 	}
 
